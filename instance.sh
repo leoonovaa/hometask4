7e6c94deed9681818539d310420bb0fc6c647f22
leoonovaa <144064947+leoonovaa@users.noreply.github.com>
2025-11-06 16:16:30 +0200

    instance.sh

--- instance.sh
+++ instance.sh
@@ -2,9 +2,8 @@ aws ec2 run-instances \
     --image-id ami-0c398cb65a93047f2 \
     --count 1 \
     --instance-type t3.micro \
-    --key-name Anna dev \
+    --key-name "Anna dev" \
     --security-group-ids sg-04370304b13203a1a \
     --subnet-id subnet-069edd1ee0ce5598a \
     --user-data file://userdata.sh \
-    --iam-instance-profile Name="ec2-profile" \
     --output text
