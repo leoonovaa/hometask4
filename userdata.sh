bfc4462dd5996aba4beaf56d3e1c3313281b2129
leoonovaa <144064947+leoonovaa@users.noreply.github.com>
2025-11-06 16:22:32 +0200

    hometask4

--- userdata.sh
+++ userdata.sh
@@ -1,15 +1,17 @@
-apt update -y && apt install -y acl
+#!/bin/bash
+sudo apt update -y && apt install -y acl
 
-adduser --disabled-password --gecos "" adminuser
-echo 'adminuser:Admin123!' | chpasswd
-usermod -aG sudo adminuser
+sudo adduser --disabled-password --gecos "" adminuser
+echo 'adminuser:Admin123!' | sudo chpasswd
+sudo usermod -aG sudo adminuser
 
-adduser --disabled-password --gecos "" poweruser
+sudo adduser --disabled-password --gecos "" poweruser
+sudo passwd -d poweruser
 
-echo "poweruser ALL=NOPASSWD: /sbin/iptables" | tee /etc/sudoers.d/poweruser
-chmod 0440 /etc/sudoers.d/poweruser
+echo "poweruser ALL=NOPASSWD: /sbin/iptables" | sudo tee /etc/sudoers.d/poweruser
+sudo chmod 0440 /etc/sudoers.d/poweruser
 
-chmod 700 /home/adminuser
-setfacl -m u:poweruser:rx /home/adminuser
+sudo chmod 700 /home/adminuser
+sudo setfacl -m u:poweruser:rx /home/adminuser
 
 sudo -u poweruser ln -s /etc/mtab /home/poweruser/mtab_link
